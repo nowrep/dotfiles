9f6bc023b0a3fff0223180fc1f8d27a957cf0a60
David Rosca <nowrep@gmail.com>
2018-02-16 17:25:03 +0100

    Czech keyboard layout for weechat

--- i3/.config/i3/keyboard-layout-switcher.py
+++ i3/.config/i3/keyboard-layout-switcher.py
@@ -3,6 +3,13 @@
 import i3ipc
 from xkbgroup import XKeyboard
 
+CZ_LAYOUT = 0
+US_LAYOUT = 1
+
+cz_window_instances = [
+    "Termite-weechat"
+]
+
 us_window_classes = [
     "krdc",
     "konsole",
@@ -15,10 +22,12 @@ xkb = XKeyboard()
 
 def on_window_focus(i3, e):
     focused = i3.get_tree().find_focused()
-    if focused.window_class in us_window_classes:
-        xkb.group_num = 1
+    if focused.window_instance in cz_window_instances:
+        xkb.group_num = CZ_LAYOUT
+    elif focused.window_class in us_window_classes:
+        xkb.group_num = US_LAYOUT
     else:
-        xkb.group_num = 0
+        xkb.group_num = CZ_LAYOUT
     # print(focused.window_class)
     # print('Focused window %s is on workspace %s' % (focused.name, focused.workspace().name))
 
