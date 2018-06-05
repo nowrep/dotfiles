bda6e8eb80c0c19d34e7cad008416b2aeaee3adc
David Rosca <nowrep@gmail.com>
2018-06-05 12:42:39 +0200

    Update i3 config

--- i3/.config/i3/startup.sh
+++ i3/.config/i3/startup.sh
@@ -3,6 +3,3 @@
 xset r rate 200 20
 setxkbmap -layout cz,us -option caps:ctrl_modifier,grp:sclk_toggle
 feh --no-fehbg --bg-fill ~/Pictures/wallpaper.png
-
-touchpad=`xinput list --name-only | grep TouchPad`
-xinput disable "$touchpad"
