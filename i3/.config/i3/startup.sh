10f4ac800304f55181ee0f1121da2fd0b8223498
David Rosca <nowrep@gmail.com>
2018-09-09 09:01:47 +0200

    Swap alt and super key

--- i3/.config/i3/startup.sh
+++ i3/.config/i3/startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 xset r rate 200 20
-setxkbmap -layout cz,us -option caps:ctrl_modifier,grp:sclk_toggle
+setxkbmap -layout cz,us -option caps:ctrl_modifier,grp:sclk_toggle,altwin:swap_alt_win
 feh --no-fehbg --bg-fill ~/Pictures/wallpaper.png
 
 touchpad=`xinput list --name-only | grep TouchPad`
