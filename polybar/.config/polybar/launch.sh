ef9220f489ad443a0a9777bc45448f18f37b0ee0
David Rosca <nowrep@gmail.com>
2021-07-06 12:23:40 +0200

    polybar: Setup GPU temperature
    Also fix persistent hwmon paths

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,5 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+export HWMON_CPU_PATH=$(echo /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input)
+export HWMON_GPU_PATH=$(echo /sys/class/drm/card1/device/hwmon/hwmon*/temp2_input)
+
 # Launch main bar
 polybar main &
