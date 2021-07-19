c50e977ea48313742e0f2282f3beeb067563d228
David Rosca <nowrep@gmail.com>
2021-07-19 19:36:56 +0200

    rofi: Update emoji-menu.sh for wayland

--- rofi/.config/rofi/emoji/emoji-menu.sh
+++ rofi/.config/rofi/emoji/emoji-menu.sh
@@ -7,12 +7,13 @@ cd $(dirname $(readlink -f $0))
 line=$(cat emoji-data | rofi -dmenu -i -p emoji)
 if [ ! -z "$line" ]; then
     char=$(cut -f 1 -d' ' <<< "$line")
-    xdotool key $(cut -f 2 <<< "$line")
-    if command -v xsel >/dev/null; then
-        echo -n "$char" | xsel -i -p
-        echo -n "$char" | xsel -i -b
-    elif command -v xclip >/dev/null; then
-        echo -n "$char" | xsel -i -sel prim
-        echo -n "$char" | xsel -i -sel clip
-    fi
+    # wtype $(cut -f 2 <<< "$line")
+    wtype $char
+    # if command -v xsel >/dev/null; then
+    #     echo -n "$char" | xsel -i -p
+    #     echo -n "$char" | xsel -i -b
+    # elif command -v xclip >/dev/null; then
+    #     echo -n "$char" | xsel -i -sel prim
+    #     echo -n "$char" | xsel -i -sel clip
+    # fi
 fi
