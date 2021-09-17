6bee94bf944d6f09f5b936fe80aaa315d8e14426
David Rosca <nowrep@gmail.com>
2021-09-17 14:35:09 +0200

    sway: Use grimshot for screenshots

--- sway/.config/sway/screenshot.sh
+++ sway/.config/sway/screenshot.sh
@@ -5,10 +5,9 @@ name=$(date +"%Y-%m-%d_%H:%M:%S.png")
 fullname="$dir/$name"
 
 if [ "$1" = "window" ]; then
-    geometry=$(swaymsg -t get_tree | jq -j '.. | select(.type?) | select(.focused) | "\(.rect.x + .window_rect.x),\(.rect.y + .window_rect.y) \(.window_rect.width)x\(.window_rect.height)"')
-    grim -g "$geometry" "$fullname"
+    /usr/share/sway/scripts/grimshot save active "$fullname"
 else
-    grim "$fullname"
+    /usr/share/sway/scripts/grimshot save output "$fullname"
 fi
 
 notify-send -i "$fullname" "Screenshot saved" "$name"
