4b8d4eed5fc3624e81fd5cfe50a38fd0eb9a8a51
David Rosca <nowrep@gmail.com>
2024-03-22 10:56:18 +0100

    Update waybar

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
 
 mkdir -p /tmp/waybar
 ln -sf /sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon*/temp2_input /tmp/waybar/cpu_temp
-ln -sf /sys/class/drm/card0/device/hwmon/hwmon*/temp2_input /tmp/waybar/gpu_temp
+ln -sf /sys/devices/pci0000:00/0000:00:03.1/0000:08:00.0/0000:09:00.0/0000:0a:00.0/hwmon/hwmon*/temp2_input /tmp/waybar/gpu_temp
 
 # Launch main bar
 waybar &
